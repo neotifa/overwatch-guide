b21d04a8ea01ccec5263f531e94f1b78ab871932
Erica Boyer <erica.boyer29@gmail.com>
2018-08-23 21:24:25 -0400

    created heroes controller

---
+++ app/controllers/heroes_controller.rb
@@ -0,0 +1,82 @@
+class HeroesController < ApplicationController
+  def Ana
+  end
+
+  def Bastion
+  end
+
+  def Brigitte
+  end
+
+  def DVa
+  end
+
+  def Doomfist
+  end
+
+  def Genji
+  end
+
+  def Hanzo
+  end
+
+  def Junkrat
+  end
+
+  def Lucio
+  end
+
+  def McCree
+  end
+
+  def Mei
+  end
+
+  def Mercy
+  end
+
+  def Moira
+  end
+
+  def Orisa
+  end
+
+  def Pharah
+  end
+
+  def Reaper
+  end
+
+  def Reinhardt
+  end
+
+  def Roadhog
+  end
+
+  def Soldier76
+  end
+
+  def Sombra
+  end
+
+  def Symmetra
+  end
+
+  def Torbjorn
+  end
+
+  def Tracer
+  end
+
+  def Widowmaker
+  end
+
+  def Winston
+  end
+
+  def Zarya
+  end
+
+  def Zenyatta
+  end
+end
